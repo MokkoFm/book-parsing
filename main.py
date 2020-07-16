71ff54a83e3ac0ece9081967ae4933859c0b49e0
MokkoFm <mokko2070@mail.ru>
2020-07-17 00:28:49 +0300

    download book covers.

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ import os.path
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
 
 os.chdir("C:/Users/mokko/Desktop/book-parsing")
 
+def download_image(id_number):
+    while id_number <=10:
+        try:
+            url = 'http://tululu.org/shots/{}.jpg'.format(id_number)
+            response = requests.get(url)
+            response.raise_for_status()
+            filename = Path('images', sanitize_filename('{}.jpg').format(id_number))
 
-def download_books(id_number):
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+
+        except requests.exceptions.HTTPError:
+            print('The book with id {} has no cover'.format(id_number))
+
+        id_number +=1
+            
+
+def download_book(id_number):
         while id_number <= 10:
             url = 'http://tululu.org/b{}/'.format(id_number)
             response = requests.get(url)
@@ -16,7 +33,13 @@ def download_books(id_number):
             soup = BeautifulSoup(response.text, 'lxml')
             find_title = soup.find('h1')
             title = find_title.text.strip(':').rsplit(':')[0]
-            
+            print('Заголовок: ' + title)            
+            try:
+                find_cover = soup.find('div', class_='bookimage').find('a').find('img')['src']
+                print(urljoin('http://tululu.org/', find_cover))
+            except AttributeError:
+                print('Attribute error')
+
             url_to_download = "http://tululu.org/txt.php"
             payload = {'id': id_number}
             response = requests.get(url_to_download, params=payload, allow_redirects=False)
@@ -26,22 +49,28 @@ def download_books(id_number):
             with open(filename, 'wb') as file:
                 file.write(response.content)
 
+            if os.stat(filename).st_size == 0:  
+                print(" Removing ",filename)  
+                os.remove(filename)  
+
             id_number += 1
 
 
 def get_information_about_book(id_number):
-        url = 'http://tululu.org/b{}/'.format(id_number)
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'lxml')
-        find_title = soup.find('h1')
-        title = find_title.text.rsplit('::')
-        find_author = soup.find('h1').find('a')
-        author = find_author.text
-        print(title)
+            url = 'http://tululu.org/b{}/'.format(id_number)
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'lxml')
+            find_title = soup.find('h1')
+            title = find_title.text.strip(':').rsplit(':')[0]
+            #find_cover = soup.find('div', class_='bookimage').find('img')['src']
+            #print(find_cover)
+            print('Заголовок: ' + title)
+
 
 def main():
-        download_books(1)
+        #download_book(1)
+        download_image(1)
         #get_information_about_book(1)
 
 
